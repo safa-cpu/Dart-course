81c60375306e603b22b99092a0372ca4b9dc26b7
safa-cpu <safapink.42@gmail.com>
2024-06-28 19:23:15 +0500

    Creating a list

---
+++ class_4/list_practice.dart
@@ -0,0 +1,12 @@
+void main(){
+// A WHOLE VARIABLE WHICH CAN CONTAIN ALL TYPE OF DATA
+var family = ["Safa","Affan","Aisha","Ghulam Hussain"];
+//THE LENGTH OF STRING IS 4 BUT IT'S ELEMENT NUMBER IS 3 I.E. (0-3)
+print (family[0]);
+// SPECIFYING A LIST WITH A STRING
+List<String> classIX = ["Safa","A","B","C","D"];
+print (classIX[1]);
+
+
+
+}
