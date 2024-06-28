5e5f243f16ea5ae8a3791346d0120dc579c730d1
safa-cpu <safapink.42@gmail.com>
2024-06-28 18:18:04 +0500

    Update nested_if.dart

--- class_4/nested_if.dart
+++ class_4/nested_if.dart
@@ -5,18 +5,4 @@
  {if (number% 5== 0 && number % 7 == 0){print("number is divisible by both");} 
  else if (number % 7 == 0){print("number is divisible by 7");}
  else {print("number is divisible by 5");}}
- else {print ("number is not divisible by 5 or 7");}
-
- 
-
-
-
-
-
-
-
-
-
-
-
-}
+ else {print ("number is not divisible by 5 or 7");}}
