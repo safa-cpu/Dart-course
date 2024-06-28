ce018c3a8a3e936fca80221882b56a2db31fe18d
safa-cpu <safapink.42@gmail.com>
2024-06-28 17:19:05 +0500

    Update if_else.dart

--- class_4/if_else.dart
+++ class_4/if_else.dart
@@ -1,8 +1,8 @@
 void main (){
 
-// int number = 21;
-//  if (number % 5 == 0){print ("number is divisible by 5");} 
-//  else {print ("number is not divisible by 5");}
+int number = 21;
+ if (number % 5 == 0){print ("number is divisible by 5");} 
+ else {print ("number is not divisible by 5");}
 
 int abc = 34;
 int a = 21;
