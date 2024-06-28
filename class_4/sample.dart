32a192ac2d6ea28109e34aedde5a28f54e20478e
safa-cpu <safapink.42@gmail.com>
2024-06-28 17:33:21 +0500

    Add code

--- class_4/sample.dart
+++ class_4/sample.dart
@@ -1,9 +1,23 @@
 void main (){
-
-
-
-
-
-
-  
+int abc = 34;
+int a = 21;
+int q = 12;
+int k = 8;
+int i = a-q; //21-12=9 i = 9
+bool w = abc>a && !(a<abc);
+// true && !(true)
+// true && false 
+// w = false
+bool e = !w || q != k ;
+// !(false) || true
+// true || true 
+// true
+bool o = e && i>4 ; 
+// true && true
+// true
+print (!(!o || e));
+//    !(!true|| true)
+//    !(false||true)
+//    !(true)
+//    false 
 }
