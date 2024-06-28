be62e7c934b5f2478a3e696026f0e0e4235800d0
safa-cpu <safapink.42@gmail.com>
2024-06-28 17:02:33 +0500

    Add initial files

---
+++ class_4/if_else.dart
@@ -0,0 +1,36 @@
+void main (){
+
+// int number = 21;
+//  if (number % 5 == 0){print ("number is divisible by 5");} 
+//  else {print ("number is not divisible by 5");}
+
+int abc = 34;
+int a = 21;
+int q = 12;
+int k = 8;
+int i = a-q; //21-12=9 i = 9
+bool w = abc>a && !(a<abc);
+// true && !(true)
+// true && false 
+// w = false
+bool e = !w || q != k ;
+// !(false) || true
+// true || true 
+// true
+bool o = e && i>4 ; 
+// true && true
+// true
+print (!(!o || e));
+//    !(!true|| true)
+//    !(false||true)
+//    !(true)
+//    false
+
+
+
+
+
+
+
+
+}
