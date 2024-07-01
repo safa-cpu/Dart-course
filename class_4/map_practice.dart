d034edd4066536b199439b9e85b4af48e000774f
safa-cpu <safapink.42@gmail.com>
2024-07-01 14:29:24 +0500

    Practice map

---
+++ class_4/map_practice.dart
@@ -0,0 +1,13 @@
+void main(){
+  // map making
+Map studentrecords = {"name":"Safa","rollNo" : "126740"};
+print (studentrecords);
+// only printing keys
+print(studentrecords.keys);
+//only printing values
+print(studentrecords.values);
+// printing only one key
+print(studentrecords["name"]);
+
+
+}
