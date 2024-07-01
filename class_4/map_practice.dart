dc53b9896d63bc4d5ced00cdd894a5d996ac6ee2
safa-cpu <safapink.42@gmail.com>
2024-07-01 19:19:22 +0500

    Adding codes

--- class_4/map_practice.dart
+++ class_4/map_practice.dart
@@ -1,6 +1,6 @@
 void main(){
   // map making
-Map studentrecords = {"name":"Safa","rollNo" : "126740"};
+Map studentrecords = {"name":"Safa","rollNo" : 126740};
 print (studentrecords);
 // only printing keys
 print(studentrecords.keys);
@@ -8,6 +8,13 @@ print(studentrecords.keys);
 print(studentrecords.values);
 // printing only one key
 print(studentrecords["name"]);
+
+List  students = [{"name":"Safa","rollNo" : 126740} ,
+{"name": "Affan","rollNo": 126741}, {"name": "Aisha", "rollNo": 126742}];
+print(students[0]);
+
+
+
 
 
 }
