fd9822e7798abdd87e6c1cb3493e2527156e5307
safa-cpu <safapink.42@gmail.com>
2024-07-01 19:20:03 +0500

    Adding more samples

--- class_4/sample.dart
+++ class_4/sample.dart
@@ -20,4 +20,30 @@ print (!(!o || e));
 //    !(false||true)
 //    !(true)
 //    false 
+
+// for each method code
+List<int> numbers = [10, 20, 30, 40, 50];
+
+numbers.forEach((number) {
+  print('The current number is: $number');
+});
+//The current number is: 10
+// The current number is: 20
+// The current number is: 30
+// The current number is: 40
+// The current number is: 50
+
+//fold and reduce
+List<int> number = [1, 2, 3, 4, 5];
+int sum = number.fold(0, (previousValue, element) => previousValue + element); // 15
+int product = number.reduce((previousValue, element) => previousValue * element); // 120
+print (sum);
+print(product);
+
+
 }
+
+
+
+
+
