8094ffd960b1ae336876eb546ec224c86688465a
safa-cpu <safapink.42@gmail.com>
2024-07-01 20:56:36 +0500

    Update map_practice

--- class_4/map_practice.dart
+++ class_4/map_practice.dart
@@ -1,6 +1,6 @@
 void main(){
   // map making
-Map studentrecords = {"name":"Safa","rollNo" : 126740};
+Map<String , dynamic> studentrecords = {"name":"Safa","rollNo" : 126740};
 print (studentrecords);
 // only printing keys
 print(studentrecords.keys);
@@ -9,9 +9,23 @@ print(studentrecords.values);
 // printing only one key
 print(studentrecords["name"]);
 
-List  students = [{"name":"Safa","rollNo" : 126740} ,
-{"name": "Affan","rollNo": 126741}, {"name": "Aisha", "rollNo": 126742}];
+List  students = [
+{"Name":["Safa","Affan"],"RollNo" : [126740,126741]} ,
+{"Name": "Affan","RollNo": 126741}, 
+{"Name": "Aisha", "RollNo": 126742}];
 print(students[0]);
+//printing all the names 
+students.forEach((student)=>print(student["Name"]));
+//length of map
+print (studentrecords.length);
+//adding an element
+studentrecords.putIfAbsent("marks", ()=>64);
+print(studentrecords);
+studentrecords.addAll({"a": "safa"});
+print(studentrecords);
+
+
+
 
 
 
