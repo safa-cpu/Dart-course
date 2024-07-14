e8a509b6a54167f54a7e22b2537544bc974098db
safa-cpu <safapink.42@gmail.com>
2024-07-14 17:23:51 +0500

    Nested loop

--- class_7/nested_loop.dart
+++ class_7/nested_loop.dart
@@ -1,12 +1,19 @@
+import 'dart:io';
+
 void main(){
-for (var i = 0; i <= 5; i++) {
- 
- for (var j = 0; j < 10; j++) {
-   print("$i : $j");
- }
-
-print ("");
-}
-
-
+// printing multiplication table for numbers
+for (var i = 1; i <= 5; i++) { print("MULTIPLICATION TABLE FOR $i:"); print("");
+for (var j = 1; j <= 10; j++) { var product = i * j ;
+print ("$i x $j = $product"); print("");}
+print("");}
+//printing patterns
+for (var k = 1; k < 5; k++) { 
+for (var o = 1; o <= k; o++) {
+stdout.write(o);} 
+// stdout.write(k)=> 
+// 1 
+// 22
+// 333
+// 4444     
+print("");}
 }
